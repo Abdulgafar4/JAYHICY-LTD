6a4547b50bb8ac9bd942ced7a7b18b38f93f6284
Abdulgafar4 <tajudeenabdulgafar4>
2024-04-08 09:42:57 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "../styles/globals.css";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Arx - Architecture Landing Page</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
